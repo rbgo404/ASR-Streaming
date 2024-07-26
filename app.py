4301990053951ead6a87eef371b1292f19d64385
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-26 16:02:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ class InferlessPythonModel:
       asr = FasterWhisperASR("en", "large-v2")
       self.online = OnlineASRProcessor(asr)
       self.audio_path = "temp.wav"
-
+      print("Current working directory:", os.getcwd(),flush=True)
   def base64_to_mp3(self, base64_data, output_file_path):
       # Convert base64 audio data to mp3 file
       mp3_data = base64.b64decode(base64_data)
       with open(output_file_path, "wb") as mp3_file:
           mp3_file.write(mp3_data)
 
-  def infer(self,inputs):
+  def infer(self,inputs,stream_output_handler):
+      #print("===> ",audio_data[:150])
       audio_data = inputs["audio_base64"]
+      print("===> "*50,audio_data[:150],flush=True)
       self.base64_to_mp3(audio_data, self.audio_path)
 
       chunk_duration = 2
@@ -29,22 +31,28 @@ class InferlessPythonModel:
           end = beg + chunk_duration
           if end > duration:
               end = duration
+          print(f"beg: {beg}, end: {end}",flush=True)
           a = load_audio_chunk(self.audio_path, beg, end)
           self.online.insert_audio_chunk(a)
-          
-          output = self.online.process_iter()
-          output_dict = {}
-          output_dict["OUT"] = output[-1]
-          stream_output_handler.send_streamed_output(output_dict)
+          try:
+              o = self.online.process_iter()
+          except AssertionError as e:
+              logger.error(f"assertion error: {e}")
+              pass
+          else:
+              output_dict = {}
+              output_dict["OUT"] = o[-1]
+              print("***"*10,o[-1],flush=True)
+              stream_output_handler.send_streamed_output(output_dict)
 
           if end >= duration:
               break
 
           beg = end
-      output = self.online.finish()
+      o = self.online.finish()
       self.online.init()
       stream_output_handler.finalise_streamed_output()
       os.remove(self.audio_path)
 
   def finalize(self):
-    self.online = None
+      self.online = None
