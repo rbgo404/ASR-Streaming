aebe31d26d349a06b7ffe2b6be5cfb81fbd3d7bb
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-29 12:13:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class InferlessPythonModel:
       with open(output_file_path, "wb") as mp3_file:
           mp3_file.write(mp3_data)
 
-  def infer(self,inputs,stream_output_handler):
+  def infer(self,inputs):
       #print("===> ",audio_data[:150])
       audio_data = inputs["audio_base64"]
       print("===> "*50,audio_data[:150],flush=True)
@@ -27,23 +27,23 @@ class InferlessPythonModel:
       beg = 0.0
       SAMPLING_RATE = 16000
       duration = len(load_audio(self.audio_path))/SAMPLING_RATE
+      output_string = ""
       while True:
           end = beg + chunk_duration
           if end > duration:
               end = duration
           print(f"beg: {beg}, end: {end}",flush=True)
           a = load_audio_chunk(self.audio_path, beg, end)
-          self.online.insert_audio_chunk(a)
+          self.online.insert_audio_chunk(a)          
           try:
               o = self.online.process_iter()
           except AssertionError as e:
               logger.error(f"assertion error: {e}")
               pass
           else:
-              output_dict = {}
-              output_dict["OUT"] = o[-1]
               print("***"*10,o[-1],flush=True)
-              stream_output_handler.send_streamed_output(output_dict)
+              output_string += o[-1]
+              # stream_output_handler.send_streamed_output(output_dict)
 
           if end >= duration:
               break
@@ -51,8 +51,9 @@ class InferlessPythonModel:
           beg = end
       o = self.online.finish()
       self.online.init()
-      stream_output_handler.finalise_streamed_output()
+      # stream_output_handler.finalise_streamed_output()
       os.remove(self.audio_path)
+      return {"recognized_text":output_string}
 
   def finalize(self):
       self.online = None
